e804fbf95a113e3acdc10990dba3b566024e4ee5
steffeno <steffeno-etc@protonmail.com>
2025-08-06 11:33:52 +0200

    nvim fix, add .local/bin to PATH

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,5 +1,9 @@
 return {
     "williamboman/mason-lspconfig",
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
     opts = function()
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
         local on_attach = function(_, bufnr)
@@ -9,20 +13,20 @@ return {
                     desc = desc
                 }
             end
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts())
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts())
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts())
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts())
-            vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts())
-            vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts())
-            vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts())
+            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Hover"))
+            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+            vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts("Signature help"))
+            vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
+            vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
             vim.keymap.set("n", "<leader>wl", function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders("List workspace folder")))
             end, opts())
-            vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts())
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts())
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts())
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts())
+            vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("View type definition"))
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename"))
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("View references"))
             vim.keymap.set("n", "<leader>f", function()
                 vim.lsp.buf.format({ async = true })
             end, opts("Format"))
@@ -35,31 +39,19 @@ return {
                 end
                 diagnostic_enabled = not diagnostic_enabled
             end
-            vim.keymap.set("n", "<leader>d", toggle_diagnostic, opts())
+            vim.keymap.set("n", "<leader>d", toggle_diagnostic, opts("Toggle diagnostics"))
         end
 
-        return {
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                        on_attach = on_attach,
-                    })
-                end,
-                ["lua_ls"] = function()
-                    require("lspconfig")["lua_ls"].setup({
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                            },
-                        },
-                    })
-                end,
+        vim.lsp.config["lua_ls"] = {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
             },
         }
-    end,
+    end
 }
